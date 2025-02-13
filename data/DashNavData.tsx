453cb483d2ec828ebb4f266e2b2b77fb2a48380a
ajagatobby <150749949+ajagatobby@users.noreply.github.com>
2025-02-13 05:52:15 +0100

    updated: dashNavData

--- data/DashNavData.tsx
+++ data/DashNavData.tsx
@@ -10,30 +10,42 @@ const DashNavData = [
     id: "analytics",
     link: "/dashboard",
     name: "Dashboard",
-    icon: (color: string) => <AnalyticsIcon color={color} />,
-    activeIcon: (color: string) => <AnalyticsIcon color={color} />,
+    icon: (color: string) => (
+      <AnalyticsIcon color={color} className="w-6 h-6" />
+    ),
+    activeIcon: (color: string) => (
+      <AnalyticsIcon color={color} className="w-6 h-6" />
+    ),
   },
   {
     id: "bot",
     link: "/dashboard/bot",
     name: "Bot",
-    icon: (color: string) => <RobotIcon color={color} />,
-    activeIcon: (color: string) => <RobotIcon color={color} />,
+    icon: (color: string) => <RobotIcon color={color} className="w-6 h-6" />,
+    activeIcon: (color: string) => (
+      <RobotIcon color={color} className="w-6 h-6" />
+    ),
   },
 
   {
     id: "account",
     link: "/dashboard/account",
     name: "Account",
-    icon: (color: string) => <UserCircleIcon color={color} />,
-    activeIcon: (color: string) => <UserCircleIcon color={color} />,
+    icon: (color: string) => (
+      <UserCircleIcon color={color} className="w-6 h-6" />
+    ),
+    activeIcon: (color: string) => (
+      <UserCircleIcon color={color} className="w-6 h-6" />
+    ),
   },
   {
     id: "settings",
     link: "/dashboard/settings",
     name: "Settings",
-    icon: (color: string) => <SettingsIcon color={color} />,
-    activeIcon: (color: string) => <SettingsIcon color={color} />,
+    icon: (color: string) => <SettingsIcon color={color} className="w-6 h-6" />,
+    activeIcon: (color: string) => (
+      <SettingsIcon color={color} className="w-6 h-6" />
+    ),
   },
 ];
 

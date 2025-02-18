d41bef5ac2cba54d6d6215639d9380b4b38efcc5
ajagatobby <150749949+ajagatobby@users.noreply.github.com>
2025-02-18 17:07:51 +0100

    updated: get auth data

--- utils/axios.ts
+++ utils/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_API_URL,
-  timeout: 10000,
+  timeout: 1000000,
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`,
